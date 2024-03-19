efce240a6a182f314cf28cdad983ecb9037d8ec9
Camilo <cam1726@hotmail.com>
2024-03-19 17:09:08 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import UserDashboard from "./components/UserDashboard/UserDashboard";
+import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Auth />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          {/* Add a default route to handle unknown URLs */}
+          <Route path="*" element={<div>hola test</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      {/* Add admin-specific content and features here */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
